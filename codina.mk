1a0610a94af20519380e8617b0d1d4eea5984968
zwliew <zhaoweiliew@gmail.com>
2014-04-19 16:48:26 -0700

    Use common storage swap

--- codina.mk
+++ codina.mk
@@ -40,10 +40,3 @@ PRODUCT_COPY_FILES += \
 # Gps
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
-
-# Storage switch script
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/SwapStorages.sh:system/xbin/SwapStorages.sh
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vold.switchablepair=/storage/sdcard0,/storage/sdcard1 \
-    persist.sys.vold.switchexternal=0
