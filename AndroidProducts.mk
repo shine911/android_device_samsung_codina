e52d6971787dfb871b5816df445a5a7b9343ef42
shine911 <deathbook1999@gmail.com>
2014-06-02 17:53:40 +0700

    Trying this way to not need patch vendor

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_codina.mk \
-    $(LOCAL_DIR)/pa_codina.mk
+ifeq (pa_codina,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/full_codina.mk \
+						 $(LOCAL_DIR)/pa_codina.mk
+endif
