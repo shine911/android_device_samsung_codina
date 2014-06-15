b096215dda3786deeaf6f269681392df63a8dab4
Robert Rozic <rooobertinhooo@gmail.com>
2014-06-15 14:57:14 +0200

    Minor updates
    -Moved storage_list.xml to common
    -Mkdir preload moved to common because janice is not using swap on preload anymore
    -Added socket for GPS
    -Update README
    -Minor comment edits xD

--- codina.mk
+++ codina.mk
@@ -17,7 +17,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.recovery.samsungcodina.rc:root/init.recovery.samsungcodina.rc \
     $(LOCAL_PATH)/rootdir/ueventd.samsungcodina.rc:root/ueventd.samsungcodina.rc
     
-# STE
+# STE Modem
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/ste_modem.sh:system/etc/ste_modem.sh
 
@@ -25,6 +25,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/adm.sqlite-u8500:system/etc/adm.sqlite-u8500
 
-# Gps
+# GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
